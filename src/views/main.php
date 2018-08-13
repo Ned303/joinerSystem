aacb3066d2e0042ed3739b7280c36ba0648f0a40
Ruan Robson <ruan.robson@a24group.com>
2018-08-13 16:58:18 +0200

    main update

--- src/views/main.php
+++ src/views/main.php
@@ -8,63 +8,62 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<style>
-		body {font-family: Arial, Helvetica, sans-serif;}
-		form {
-			border: 3px solid #f1f1f1;
-			width: 30%;
-			margin: auto;
-			border-radius: 25px;
+		body {
+			display: grid;
+			grid-template-areas:
+				"header header header"
+				"nav article article";
+			grid-template-rows: 206px 1fr;
+			grid-template-columns: 300px 1fr 15%;
+			height: 100vh;
+			margin: 0;
+			font-family: Arial, Helvetica, sans-serif;
 		}
-
-		input[type=text], input[type=password] {
-			width: 100%;
-			padding: 12px 20px;
-			margin: 8px 0;
-			display: inline-block;
-			border: 1px solid #ccc;
-			box-sizing: border-box;
+		#pageHeader {
+			grid-area: header;
 		}
-
-		button {
-			background-color: #0065A4;
-			color: white;
-			padding: 14px 20px;
-			margin: 8px 0;
-			border: none;
-			cursor: pointer;
-			width: 100%;
+		#pageFooter {
+			grid-area: footer;
 		}
-
-		button:hover {
-			opacity: 0.8;
+		#mainArticle {
+			grid-area: article;
 		}
-
-		.container {
-			padding: 16px;
-			border-radius: 25px;
-		}
-
-		.header-container {
-			width: 100%;
-			height: 100px;
-			background-color: #0065A4;
-		}
-
-		img {
-			display: block;
-			margin-left: auto;
-			margin-right: auto;
-		}
-
-		table {
-			width:100%;
-		}
-
-		.topMenu {
+		#mainNav {
+			grid-area: nav;
 			background:#0065A4;
+			color:white;
 		}
-
+		article, nav {
+			background: gold;
+		}
 	</style>
 </head>
 <body>
+	<header id="pageHeader">
+		<center>
+			<div>
+				<!-- following div loads the image-->
+				<a href="http://a24group.com/" title="a24 Group">
+					<img src="https://static1.squarespace.com/static/5149c458e4b0199d103d7411/t/5b682fb10e2e72aae8b968c9/1533554610971/logo.PNG?format=500w" alt="A24 Group">
+				</a>
+			</div>
+		</center>
+		<div style="text-align:right;background:#0065A4;color:white;width:100%;">
+			<div>
+				Welcome <?php echo $_SESSION['username']; ?>
+			</div>
+			<div>
+				Logout
+			</div>
+		</div>
+	</header>
+	<article id="mainArticle">Article</article>
+	<nav id="mainNav">
+		<center>
+		<p>Admin</p>
+		<p>Joiner</p>
+		<p>Leaver</p>
+		<p>Mover</p>
+		</center>
+	</nav>
 </body>
