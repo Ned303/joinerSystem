9f0e14fc5cedf731036cccfeebba136fb1142c02
Ruan Robson <ruan.robson@a24group.com>
2018-08-13 16:22:28 +0200

    update
    update

--- src/views/main.php
+++ src/views/main.php
@@ -1,4 +1,70 @@
 <?php
     session_start();
-    echo "Logged in as " . $_SESSION['username'];
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Joiner System</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+		body {font-family: Arial, Helvetica, sans-serif;}
+		form {
+			border: 3px solid #f1f1f1;
+			width: 30%;
+			margin: auto;
+			border-radius: 25px;
+		}
+
+		input[type=text], input[type=password] {
+			width: 100%;
+			padding: 12px 20px;
+			margin: 8px 0;
+			display: inline-block;
+			border: 1px solid #ccc;
+			box-sizing: border-box;
+		}
+
+		button {
+			background-color: #0065A4;
+			color: white;
+			padding: 14px 20px;
+			margin: 8px 0;
+			border: none;
+			cursor: pointer;
+			width: 100%;
+		}
+
+		button:hover {
+			opacity: 0.8;
+		}
+
+		.container {
+			padding: 16px;
+			border-radius: 25px;
+		}
+
+		.header-container {
+			width: 100%;
+			height: 100px;
+			background-color: #0065A4;
+		}
+
+		img {
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+		}
+
+		table {
+			width:100%;
+		}
+
+		.topMenu {
+			background:#0065A4;
+		}
+
+	</style>
+</head>
+<body>
+</body>
--- src/views/passConfirm.php
+++ src/views/passConfirm.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-    <title>Login</title>
+    <title>Joiner System</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
