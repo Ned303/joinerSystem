2095ed828d5210932f1a7eae7ac0ae545b47ff39
Ned303 <39126764+Ned303@users.noreply.github.com>
2018-09-01 19:48:48 +0200

    Update admin.php

--- src/views/Admin.php
+++ src/views/Admin.php
@@ -135,6 +135,28 @@
         </div>
     </div>
 
+    <div class="modal fade" id="passReset" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header" style="background-color: #0065A4;">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title" style="color: white">Password Reset</h4>
+                </div>
+                <form action="index.php" method="POST">
+                    <div class="modal-body">
+                        Are you sure you want to reset password
+                    </div>
+
+                    <div class="modal-footer">
+                        <input type="submit" class="btn btn-default" data-dismiss="modal" value="Yes"> <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
     <div class="modal fade" id="remove" role="dialog">
         <div class="modal-dialog">
 
@@ -142,7 +164,7 @@
             <div class="modal-content">
                 <div class="modal-header" style="background-color: #0065A4;">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title" style="color: white">Edit User</h4>
+                    <h4 class="modal-title" style="color: white">Remove Account</h4>
                 </div>
                 <form action="index.php" method="POST">
                     <div class="modal-body">
